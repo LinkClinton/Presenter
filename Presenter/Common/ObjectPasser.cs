8e27b9733c21053ab7d87044c1851c2f7a886317
Link Clinton <LinkClinton@outlook.com>
2017-06-01 14:28:02 +0800

    add objectpasser like PipelineState

---
+++ Presenter/Common/ObjectPasser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presenter
+{
+    public class ObjectPasser
+    {
+        private Buffer vertexBuffer;
+        private Buffer indexBuffer;
+        private Shader vertexShader;
+        private Shader pixelShader;
+        private BufferLayout bufferLayout;
+        
+
+        public Buffer VertexBuffer
+        {
+            set => vertexBuffer = value;
+            get => vertexBuffer;
+        }
+
+        public Buffer IndexBuffer
+        {
+            set => indexBuffer = value;
+            get => indexBuffer;
+        }
+
+        public Shader VertexShader
+        {
+            set => vertexShader = value;
+            get => vertexShader;
+        }
+
+        public Shader PixelShader
+        {
+            set => pixelShader = value;
+            get => pixelShader;
+        }
+
+        public BufferLayout BufferLayout
+        {
+            set => bufferLayout = value;
+            get => bufferLayout;
+        }
+
+    }
+}
